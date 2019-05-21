f0d5e826d6ae7875b602046913b1945a1183753f
Fabio Seiji Irokawa <fabio.irokawa1@hotmail.com>
2019-05-21 14:02:39 -0300

    Update trabalho-editado.sql

--- trabalho-editado.sql
+++ trabalho-editado.sql
@@ -2,13 +2,26 @@
 --Disciplinas: Cod, Nome, Cred
 --Cursos: Num, Nome
 
+--drop tables
+
+drop table cursam;
+drop table cursaram;
+drop table pre_req;
+drop table grade;
+
+drop table alunos_grad;
+drop table alunos_pos_grad;
+drop table disciplinas;
+drop table alunos;
+drop table cursos;
+
 --cd é a média do aluno
 
 create table alunos(ra number(4), nome_aluno varchar2(20), num_curso number, data_nasc date);
-create table disciplinas (nome_disc varchar2(30), cod_disc number, cred number);
-create table cursos (nome_curso varchar2(30), num_curso number);
+create table disciplinas (nome_disc varchar2(75), cod_disc number, cred number);
+create table cursos (nome_curso varchar2(75), num_curso number);
 
-create table alunos_grad (ra_g number(4),cd number(2,2), ); 
+create table alunos_grad (ra_g number(4),cd number(3,1)); 
 create table alunos_pos_grad (ra_p_g number(4), orientador varchar2(20)); 
 
 alter table alunos add constraint pk_ra_aluno primary key (ra);
@@ -21,7 +34,7 @@ alter table alunos_pos_grad add constraint fk_pos_grad_aluno foreign key (ra_p_g
 --relacionamentos
 
 create table cursam (ra_cursam number(4), cod_disc_cursam number, freq number);
-create table cursaram (ra_cursaram number(4), cod_disc_cursaram number, sem_ano date, nota number(2,2), freq number, status varchar2(1)); 
+create table cursaram (ra_cursaram number(4), cod_disc_cursaram number, sem_ano date, nota number(3,1), freq number, status varchar2(1)); 
 create table pre_req (cod_disc_pre number, cod_pre_req number);
 create table grade (cod_disc_grade number, num_curso_grade number);
 
@@ -32,109 +45,29 @@ alter table cursaram add constraint pk_sem_cursaram primary key (sem_ano);
 alter table pre_req add constraint pk_pre_req primary key (cod_pre_req);
 
 --adicionando chaves estrangeiras dos relacionamentos
-alter table cursam add constraint fk_ra_cursam foreign key (ra) references alunos;
-alter table cursam add constraint fK_cod_disc_cursam foreign key (cod_disc) references disciplinas;
+alter table alunos add constraint fk_num_curso foreign key (num_curso) references cursos;
 
-alter table cursaram add constraint fk_ra_cursaram foreign key (ra) references alunos;
-alter table cursaram add constraint fK_cod_disc_cursaram foreign key (cod_disc) references disciplinas;
+alter table cursam add constraint fk_ra_cursam foreign key (ra_cursam) references alunos;
+alter table cursam add constraint fK_cod_disc_cursam foreign key (cod_disc_cursam) references disciplinas;
 
-alter table pre_req add constraint fK_cod_disc_pre foreign key (cod_disc) references disciplinas;
+alter table cursaram add constraint fk_ra_cursaram foreign key (ra_cursaram) references alunos;
+alter table cursaram add constraint fK_cod_disc_cursaram foreign key (cod_disc_cursaram) references disciplinas;
 
-alter table grade add constraint fK_cod_disc_grade foreign key (cod_disc) references disciplinas;
-alter table grade add constraint fk_num_curso_grade foreign key (num_curso) references cursos;
+alter table pre_req add constraint fK_cod_disc_pre foreign key (cod_disc_pre) references disciplinas;
+alter table pre_req add constraint fK_pre_req foreign key (cod_pre_req) references disciplinas;
 
---30 alunos graduação
-insert into alunos values(1000,'Alfa',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1001,'Beta',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1002,'Charlie',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1003,'Delta',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1004,'Echo',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1005,'Foxtrot',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1006,'Golf',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1007,'Hotel',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1008,'India',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1009,'Juliet',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1010,'Kilo',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1011,'Lima',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1012,'Mike',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1013,'November',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1014,'Oscar',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1015,'Papa',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1016,'Quebec',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1017,'Romeo',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1018,'Sierra',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1019,'Tango',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1020,'Uniform',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1021,'Victor',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1022,'Whiskey',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1023,'Xray',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1024,'Yankee',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1025,'Zulu',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1026,'Wojtek',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1027,'Artyom',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(1028,'Boris',10,to_date('12/06/1998','dd/mm/yyyy'));
+alter table grade add constraint fK_cod_disc_grade foreign key (cod_disc_grade) references disciplinas;
+alter table grade add constraint fk_num_curso_grade foreign key (num_curso_grade) references cursos;
 
-
-insert into alunos_grad values(1000,7.5);
-insert into alunos_grad values(1001,4.2);
-insert into alunos_grad values(1002,2.3);
-insert into alunos_grad values(1003,7.8);
-insert into alunos_grad values(1004,8.9);
-insert into alunos_grad values(1005,1.2);
-insert into alunos_grad values(1006,3.3);
-insert into alunos_grad values(1007,4.5);
-insert into alunos_grad values(1008,7.6);
-insert into alunos_grad values(1009,5.6);
-insert into alunos_grad values(1010,2.3);
-insert into alunos_grad values(1011,9.8);
-insert into alunos_grad values(1012,9.7);
-insert into alunos_grad values(1013,1.1);
-insert into alunos_grad values(1014,3.3);
-insert into alunos_grad values(1015,4.3);
-insert into alunos_grad values(1016,5.4);
-insert into alunos_grad values(1017,6.5);
-insert into alunos_grad values(1018,6.6);
-insert into alunos_grad values(1019,7.1);
-insert into alunos_grad values(1020,8.3);
-insert into alunos_grad values(1021,4.3);
-insert into alunos_grad values(1022,4.9);
-insert into alunos_grad values(1023,0.1);
-insert into alunos_grad values(1024,0.9);
-insert into alunos_grad values(1025,7.0);
-insert into alunos_grad values(1026,5.0);
-insert into alunos_grad values(1027,4.0);
-insert into alunos_grad values(1028,10.0);
-
-
---10 alunos pós graduação
-insert into alunos values(2000,'Hanzo',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(2001,'Genji',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(2002,'Junkrat',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(2003,'Winston',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(2004,'Zarya',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(2005,'Mercy',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(2006,'Roadhog',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(2007,'Reinhardt',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(2008,'Zenyatta',10,to_date('12/06/1998','dd/mm/yyyy'));
-insert into alunos values(2009,'Tracer',10,to_date('12/06/1998','dd/mm/yyyy'));
-
-insert into alunos_pos_grad values(2000,'Jayce');
-insert into alunos_pos_grad values(2001,'Elise');
-insert into alunos_pos_grad values(2002,'Zoe');
-insert into alunos_pos_grad values(2003,'Braum');
-insert into alunos_pos_grad values(2004,'Sylas');
-insert into alunos_pos_grad values(2005,'Ezreal');
-insert into alunos_pos_grad values(2006,'Pyke');
-insert into alunos_pos_grad values(2007,'Syndra');
-insert into alunos_pos_grad values(2008,'Varus');
-insert into alunos_pos_grad values(2009,'Teemo');
-
---30 disciplinas
---create table disciplinas (nome_disc varchar2(30), cod_disc number, cred number);
+--create table cursos (nome_curso varchar2(30), num_curso number);
+insert into cursos values ('Engenharia de Computação', 10);   
+insert into cursos values ('Administração', 20);
+insert into cursos values ('Medicina', 30);
+insert into cursos values ('Direito', 40);
 
 --disciplinas Eng
 insert into disciplinas values ('ALGORÍTMOS E PROGRAMAÇÃO DE COMPUTADORES A', 0001, 04);
-insert into disciplinas values ('ALGORÍTMOS E PROGRAMAÇÃO DE COMPUTADORES B', 0002, 046;
+insert into disciplinas values ('ALGORÍTMOS E PROGRAMAÇÃO DE COMPUTADORES B', 0002, 04);
 insert into disciplinas values ('ANTROPOLOGIA TEOLÓGICA A', 0003, 02);
 insert into disciplinas values ('ANTROPOLOGIA TEOLÓGICA B', 0004, 02);
 insert into disciplinas values ('ANTROPOLOGIA TEOLÓGICA C', 0005, 02);
@@ -229,9 +162,122 @@ insert into disciplinas values ('DIREITO DO TRABALHO II', 0088, 04);
 insert into disciplinas values ('DIREITO COMERCIAL I', 0089, 04);
 insert into disciplinas values ('DIREITO COMERCIAL II', 0090, 04);
 
---create table cursos (nome_curso varchar2(30), num_curso number);
-insert into cursos values ('Engenharia de Computação', 01);   
-insert into cursos values ('Administração', 02);
-insert into cursos values ('Medicina', 03);
-insert into cursos values ('Direito', 04);
+--30 alunos graduação
+insert into alunos values(1000,'Alfa',10,to_date('12/08/1998','dd/mm/yyyy'));
+insert into alunos values(1001,'Beta',10,to_date('21/09/1998','dd/mm/yyyy'));
+insert into alunos values(1002,'Charlie',20,to_date('11/06/1998','dd/mm/yyyy'));
+insert into alunos values(1003,'Delta',30,to_date('15/10/1998','dd/mm/yyyy'));
+insert into alunos values(1004,'Echo',40,to_date('17/12/1998','dd/mm/yyyy'));
+insert into alunos values(1005,'Foxtrot',40,to_date('19/11/1998','dd/mm/yyyy'));
+insert into alunos values(1006,'Golf',40,to_date('10/02/1998','dd/mm/yyyy'));
+insert into alunos values(1007,'Hotel',40,to_date('22/02/1998','dd/mm/yyyy'));
+insert into alunos values(1008,'India',30,to_date('29/03/1998','dd/mm/yyyy'));
+insert into alunos values(1009,'Juliet',20,to_date('11/04/1998','dd/mm/yyyy'));
+insert into alunos values(1010,'Kilo',10,to_date('01/05/1998','dd/mm/yyyy'));
+insert into alunos values(1011,'Lima',20,to_date('02/07/1998','dd/mm/yyyy'));
+insert into alunos values(1012,'Mike',20,to_date('06/06/1998','dd/mm/yyyy'));
+insert into alunos values(1013,'November',20,to_date('09/01/1998','dd/mm/yyyy'));
+insert into alunos values(1014,'Oscar',30,to_date('10/01/1998','dd/mm/yyyy'));
+insert into alunos values(1015,'Papa',40,to_date('04/03/1998','dd/mm/yyyy'));
+insert into alunos values(1016,'Quebec',30,to_date('06/04/1998','dd/mm/yyyy'));
+insert into alunos values(1017,'Romeo',20,to_date('11/05/1998','dd/mm/yyyy'));
+insert into alunos values(1018,'Sierra',10,to_date('10/09/1998','dd/mm/yyyy'));
+insert into alunos values(1019,'Tango',20,to_date('11/11/1998','dd/mm/yyyy'));
+insert into alunos values(1020,'Uniform',40,to_date('02/12/1998','dd/mm/yyyy'));
+insert into alunos values(1021,'Victor',40,to_date('04/07/1998','dd/mm/yyyy'));
+insert into alunos values(1022,'Whiskey',20,to_date('08/04/1998','dd/mm/yyyy'));
+insert into alunos values(1023,'Xray',20,to_date('09/03/1998','dd/mm/yyyy'));
+insert into alunos values(1024,'Yankee',30,to_date('10/02/1998','dd/mm/yyyy'));
+insert into alunos values(1025,'Zulu',30,to_date('11/06/1998','dd/mm/yyyy'));
+insert into alunos values(1026,'Wojtek',30,to_date('10/01/1998','dd/mm/yyyy'));
+insert into alunos values(1027,'Artyom',20,to_date('14/08/1998','dd/mm/yyyy'));
+insert into alunos values(1028,'Boris',20,to_date('19/09/1998','dd/mm/yyyy'));
+
+--select * from alunos where cd is 7.5;
+
+insert into alunos_grad values(1000,7.5);
+insert into alunos_grad values(1001,4.2);
+insert into alunos_grad values(1002,2.3);
+insert into alunos_grad values(1003,7.8);
+insert into alunos_grad values(1004,8.9);
+insert into alunos_grad values(1005,1.2);
+insert into alunos_grad values(1006,3.3);
+insert into alunos_grad values(1007,4.5);
+insert into alunos_grad values(1008,7.6);
+insert into alunos_grad values(1009,5.6);
+insert into alunos_grad values(1010,2.3);
+insert into alunos_grad values(1011,9.8);
+insert into alunos_grad values(1012,9.7);
+insert into alunos_grad values(1013,1.1);
+insert into alunos_grad values(1014,3.3);
+insert into alunos_grad values(1015,4.3);
+insert into alunos_grad values(1016,5.4);
+insert into alunos_grad values(1017,6.5);
+insert into alunos_grad values(1018,6.6);
+insert into alunos_grad values(1019,7.1);
+insert into alunos_grad values(1020,8.3);
+insert into alunos_grad values(1021,4.3);
+insert into alunos_grad values(1022,4.9);
+insert into alunos_grad values(1023,0.1);
+insert into alunos_grad values(1024,0.9);
+insert into alunos_grad values(1025,7.0);
+insert into alunos_grad values(1026,5.0);
+insert into alunos_grad values(1027,4.0);
+insert into alunos_grad values(1028,10.0);
+
+--10 alunos pós graduação
+insert into alunos values(2000,'Hanzo',10,to_date('11/06/1998','dd/mm/yyyy'));
+insert into alunos values(2001,'Genji',10,to_date('11/07/1998','dd/mm/yyyy'));
+insert into alunos values(2002,'Junkrat',20,to_date('05/09/1998','dd/mm/yyyy'));
+insert into alunos values(2003,'Winston',20,to_date('08/11/1998','dd/mm/yyyy'));
+insert into alunos values(2004,'Zarya',30,to_date('18/01/1998','dd/mm/yyyy'));
+insert into alunos values(2005,'Mercy',30,to_date('19/02/1998','dd/mm/yyyy'));
+insert into alunos values(2006,'Roadhog',40,to_date('21/04/1998','dd/mm/yyyy'));
+insert into alunos values(2007,'Reinhardt',40,to_date('29/09/1998','dd/mm/yyyy'));
+insert into alunos values(2008,'Zenyatta',20,to_date('21/10/1998','dd/mm/yyyy'));
+insert into alunos values(2009,'Tracer',10,to_date('03/12/1998','dd/mm/yyyy'));
+
+insert into alunos_pos_grad values(2000,'Jayce');
+insert into alunos_pos_grad values(2001,'Elise');
+insert into alunos_pos_grad values(2002,'Zoe');
+insert into alunos_pos_grad values(2003,'Braum');
+insert into alunos_pos_grad values(2004,'Sylas');
+insert into alunos_pos_grad values(2005,'Ezreal');
+insert into alunos_pos_grad values(2006,'Pyke');
+insert into alunos_pos_grad values(2007,'Syndra');
+insert into alunos_pos_grad values(2008,'Varus');
+insert into alunos_pos_grad values(2009,'Teemo');
+
+--30 disciplinas
+--create table disciplinas (nome_disc varchar2(30), cod_disc number, cred number);
+
+
+
+
+
+create or replace procedure historico_escolar (ra number)
+begin
+for ra_busca in (select * from alunos)
+	loop
+	if ra = ra_busca.ra then
+	for ra_busca_grad in (select * from alunos_grad)
+		loop
+		if ra = ra_busca_grad then
+		dbms_output.put_line(ra_busca.nome_aluno,ra_busca_grad.cd);
+		end if;
+		end loop;
+	else
+	for ra_busca_pos_grad in (select * from alunos_pos_grad)
+		loop
+		if ra = ra_busca_pos_grad then
+		dbms_output.put_line(ra_busca.nome_aluno,ra_busca_pos_grad.orientador);
+		end if;
+		end loop;
+		
+	end if;
+	end loop;
+		
+
+
+
 
